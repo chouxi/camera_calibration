e34c83f4d05df790fb745067a2e58fbb30984911
chouxi <qizheng1993hit@gmail.com>
2017-04-13 16:38:38 -0400

    finish part1

--- camera_calibration_3d.m
+++ camera_calibration_3d.m
@@ -28,4 +28,35 @@ P
 
 %% Calculate M by SVD
 [U,S,V] = svd(P);
-M=[(V(1:4,12))';(V(5:8,12))';(V(8:11,12))']
+M=[(V(1:4,12))';(V(5:8,12))';(V(9:12,12))']
+
+%% Calculate camera center by SVD
+[m_U, m_S, m_V] = svd(M);
+t=m_V(:,end);
+center=t(1:end-1)/t(end)
+
+%% Calculate M'
+M_prime = M(:,1:3)/ M(3,3)
+
+%% Calculate R_x
+cos_x=M_prime(3,3)/sqrt(M_prime(3,2)^2 + M_prime(3,3)^2);
+sin_x=-M_prime(3,2)/sqrt(M_prime(3,2)^2 + M_prime(3,3)^2);
+R_x=[1, 0, 0;
+    0, cos_x, -sin_x;
+    0, sin_x, cos_x;]
+theta_x = asin(sin_x)*180/pi
+N=M_prime*R_x
+
+%% Calculate R_z
+cos_z = N(2,2)/sqrt(N(2,1)^2 + N(2,2)^2);
+sin_z = -N(2,1)/sqrt(N(2,1)^2 + N(2,2)^2);
+R_z = [cos_z, -sin_z, 0;
+        sin_z, cos_z, 0;
+        0, 0, 1]
+theta_z = asin(sin_z)*180/pi
+
+%% Calculate K by M'/(R_x*R_y*R_z)
+K = M_prime/(R_x*diag([1,1,1])*R_z);
+K = K/K(3,3)
+fcoal=[K(1,1),K(2,2)]
+img_center=[K(1,3),K(2,3)]
