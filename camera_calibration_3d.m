7ed70a1286cdcdce1a16f8a8e341fb1e3f978fe3
chouxi <qizheng1993hit@gmail.com>
2017-04-13 16:56:18 -0400

    add some comment

--- camera_calibration_3d.m
+++ camera_calibration_3d.m
@@ -1,4 +1,5 @@
 %% Camera Calibration using 3D calibration object
+% Zheng Qi
 %% Draw the image points
 
 real_points = [2 2 2;
@@ -22,16 +23,20 @@ grid on
 [x, y] = size(real_points);
 P=[];
 for i=1:x
+    % In each iteration add 2 rows.
     P=[P; gen_mat_P(real_points(i,:),pixel_points(i,:))];
 end
 P
 
 %% Calculate M by SVD
 [U,S,V] = svd(P);
+% Got M matrix from last col of V
 M=[(V(1:4,12))';(V(5:8,12))';(V(9:12,12))']
 
 %% Calculate camera center by SVD
 [m_U, m_S, m_V] = svd(M);
+% t is the last col of V of SVD of M,
+% center got from rescaling the t with setting the last one of t to 1
 t=m_V(:,end);
 center=t(1:end-1)/t(end)
 
@@ -44,6 +49,8 @@ sin_x=-M_prime(3,2)/sqrt(M_prime(3,2)^2 + M_prime(3,3)^2);
 R_x=[1, 0, 0;
     0, cos_x, -sin_x;
     0, sin_x, cos_x;]
+% The asin() in matlab is arsin, and the result is radian.
+% We need to change it to degree.
 theta_x = asin(sin_x)*180/pi
 N=M_prime*R_x
 
@@ -56,6 +63,7 @@ R_z = [cos_z, -sin_z, 0;
 theta_z = asin(sin_z)*180/pi
 
 %% Calculate K by M'/(R_x*R_y*R_z)
+% R_y is a diagnal matrix with the values are 1 on the diagnal.
 K = M_prime/(R_x*diag([1,1,1])*R_z);
 K = K/K(3,3)
 fcoal=[K(1,1),K(2,2)]
