6717bdb78426ccd80635986c8e30e31796529a6b
chouxi <qizheng1993hit@gmail.com>
2017-04-14 16:35:44 -0400

    intrinsic matrix

--- camera_calibration_2d.m
+++ camera_calibration_2d.m
@@ -40,5 +40,15 @@ B=[b(1) b(2) b(4);
    b(2) b(3) b(5);
    b(4) b(5) b(6)]
 
-%% Computing inrinsic
+%% Computing intrinsic
 v_0=(B(1,1)*B(1,3)-B(1,1)*B(2,3))/(B(1,1)*B(2,2)-B(1,2)^2);
+lamda=B(3,3) - (B(1,3)^2 + v_0*(B(1,2)*B(1,3)-B(1,1)*B(2,3)))/B(1,1);
+alpha=sqrt(lamda/B(1,1));
+beta=sqrt(lamda*B(1,1)/(B(1,1)*B(2,2)-B(1,2)^2));
+gamma=-B(1,2)*(alpha^2)*beta/lamda;
+u_0=gamma*v_0/alpha-B(1,3)*(alpha^2)/lamda;
+A=[alpha gamma u_0;
+   0 beta v_0;
+   0 0 1]
+
+%% Computing rotation matrix
