ca16fd904907affcff941fd5bda298bec9fe98d0
chouxi <qizheng1993hit@gmail.com>
2017-04-19 14:26:58 -0400

    improving accuarcy finish

--- camera_calibration_2d.m
+++ camera_calibration_2d.m
@@ -48,20 +48,20 @@ for i=1:len
 	V=[V; get_v(homo)];
 end
 
+%% Computing intrinsic
 [V_U, V_S, V_V] = svd(V);
 b=V_V(:,end);
 B=[b(1) b(2) b(4);
    b(2) b(3) b(5);
    b(4) b(5) b(6)]
 
-%% Computing intrinsic
 v_0=(B(1,1)*B(1,3)-B(1,1)*B(2,3))/(B(1,1)*B(2,2)-B(1,2)^2);
 lamda=B(3,3) - (B(1,3)^2 + v_0*(B(1,2)*B(1,3)-B(1,1)*B(2,3)))/B(1,1);
 alpha=sqrt(lamda/B(1,1));
 beta=sqrt(lamda*B(1,1)/(B(1,1)*B(2,2)-B(1,2)^2));
 gamma=-B(1,2)*(alpha^2)*beta/lamda;
 u_0=gamma*v_0/alpha-B(1,3)*(alpha^2)/lamda;
-A=[alpha gamma u_0;
+K=[alpha gamma u_0;
    0 beta v_0;
    0 0 1]
 
@@ -69,10 +69,10 @@ A=[alpha gamma u_0;
 for i=1:len
 	file_name = char(file_list(1,i))
 	homo = homo_list(i*3-2:i*3,:);
-	r_1=lamda*inv(A)* homo(:,1);
-	r_2=lamda*inv(A) * homo(:,2);
+	r_1=lamda*inv(K)* homo(:,1);
+	r_2=lamda*inv(K) * homo(:,2);
 	R =[r_1 r_2 cross(r_1, r_2)]
-	t = lamda*inv(A) *homo(:,3)
+	t = lamda*inv(K) *homo(:,3)
 	R_T_R = R'*R
 	%%
 	% get new R by SVD
@@ -81,6 +81,9 @@ for i=1:len
 	new_R_T_R=new_R'*new_R
 end
 %% Improving Accuracy
+H_list =[];
+Acc_R_list =[];
+Acc_t_list =[];
 for i=1:len
 	file_name = char(file_list(1,i))
 	homo = homo_list(i*3-2:i*3,:);
@@ -111,7 +114,6 @@ for i=1:len
 		[min_val row_idx] = min(n);
 		row_list(j) = row_idx;
 	end
-	row_list
 	p_correct=harris_corner(row_list(:),:);
 	figure
 	image=imread(file_name);
@@ -120,4 +122,47 @@ for i=1:len
 	title('Figure 3 : grid points')
 	plot(p_correct(:,1), p_correct(:,2), '+');
 	pause(0.5)
+	%%% Step 4
+	p_correct = [p_correct'; ones(1,size(p_correct,1))]
+    H = homography2d(real_corners, p_correct)
+    H_list = [H_list; H/H(end,end)];
+	%%% Step 6 Sum of Square Error
+	image_corner = H*real_corners;
+	image_corner = image_corner ./ repmat(image_corner(3,:),size(p_approx,1), 1)
+	err_reprojection = sqrt(sum(sum((image_corner-p_correct).^2))) / size(image_corner,2)
+end
+%%% Step 5 We get step 6 before 5
+Acc_V=[];
+for i=1:len
+	homo = H_list(i*3-2:i*3,:);
+	Acc_V=[Acc_V; get_v(homo)];
+end
+[V_U, V_S, V_V] = svd(V);
+b=V_V(:,end);
+B=[b(1) b(2) b(4);
+   b(2) b(3) b(5);
+   b(4) b(5) b(6)];
+
+v_0=(B(1,1)*B(1,3)-B(1,1)*B(2,3))/(B(1,1)*B(2,2)-B(1,2)^2);
+lamda=B(3,3) - (B(1,3)^2 + v_0*(B(1,2)*B(1,3)-B(1,1)*B(2,3)))/B(1,1);
+alpha=sqrt(lamda/B(1,1));
+beta=sqrt(lamda*B(1,1)/(B(1,1)*B(2,2)-B(1,2)^2));
+gamma=-B(1,2)*(alpha^2)*beta/lamda;
+u_0=gamma*v_0/alpha-B(1,3)*(alpha^2)/lamda;
+K=[alpha gamma u_0;
+   0 beta v_0;
+   0 0 1]
+for i=1:len
+	file_name = char(file_list(1,i))
+	homo = H_list(i*3-2:i*3,:);
+	r_1=lamda*inv(K)* homo(:,1);
+	r_2=lamda*inv(K) * homo(:,2);
+	R =[r_1 r_2 cross(r_1, r_2)];
+	%%
+	% get new R by SVD
+	[R_U, R_S, R_V] = svd(R);
+	R = R_U*R_V'
+	Acc_R_list = [Acc_R_list;R];
+	t = lamda*inv(K) *homo(:,3)
+	Acc_t_list = [Acc_t_list;t];
 end
