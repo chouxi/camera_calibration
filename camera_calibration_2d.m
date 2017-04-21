7c8735f14e7bc7b140c7b82ee115816a8245bcb2
chouxi <qizheng1993hit@gmail.com>
2017-04-21 15:04:33 -0400

    little update

--- camera_calibration_2d.m
+++ camera_calibration_2d.m
@@ -56,7 +56,7 @@ B=[b(1) b(2) b(4);
    b(4) b(5) b(6)]
 
 v_0=(B(1,2)*B(1,3)-B(1,1)*B(2,3))/(B(1,1)*B(2,2)-B(1,2)^2);
-lamda=B(3,3) - (B(1,3)^2 + v_0*(B(1,2)*B(1,3)-B(1,1)*B(2,3)))/B(1,1);
+lamda=B(3,3) - (B(1,3)^2 + v_0*(B(1,2)*B(1,3)-B(1,1)*B(2,3)))/B(1,1)
 alpha=sqrt(lamda/B(1,1));
 beta=sqrt(lamda*B(1,1)/(B(1,1)*B(2,2)-B(1,2)^2));
 gamma=-B(1,2)*(alpha^2)*beta/lamda;
@@ -157,7 +157,7 @@ for i=1:len
 	homo = H_list(i*3-2:i*3,:);
 	r_1=lamda*inv(K)* homo(:,1);
 	r_2=lamda*inv(K) * homo(:,2);
-	R =[r_1 r_2 cross(r_1, r_2)];
+	R =[r_1 r_2 cross(r_1, r_2)]
 	Acc_R_list{i} = R;
 	t = lamda*inv(K) *homo(:,3)
 	Acc_t_list{i} = t;
